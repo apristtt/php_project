f236a702824194b47c61e9e1c556a07f35c25b4b
dell <dell@ubuntu.ubuntu-domain>
2014-02-16 17:03:18 +0700

    On ubuntu

--- displaySidebar.php
+++ displaySidebar.php
@@ -1,5 +1,5 @@
             <div class="col-lg-3">
-                <h4>News Management</h4>
+                <h4>Site Management</h4>
                 <div class="panel panel-default">
                     <div class="panel-body">
                         <div class="form-group">
@@ -10,9 +10,13 @@
                         </a>
                         </div>
                         <div class="form-group">
+                        <?
+                            $queryTotalHiddenNews = mysql_query("SELECT * FROM News WHERE NewsHidden = 1") or die(mysql_error());
+                            $numTotalHiddenNews = mysql_num_rows($queryTotalHiddenNews);
+                        ?>
                         <a href="viewHiddenNews.php">
                             <button type="button" class="btn btn-warning">
-                                <span class="glyphicon glyphicon-eye-open"></span> View Hidden News
+                                <span class="glyphicon glyphicon-eye-open"></span> <span class="badges"><? echo $numTotalHiddenNews ?></span> Hidden News
                             </button>
                         </a>
                         </div>
@@ -23,7 +27,7 @@
                 <div class="list-group">
 
                     <?php
-                        $resultRecentNews = mysql_query("SELECT * FROM News WHERE NewsPinned = 0 ORDER by NewsID DESC") or die(mysql_error());
+                        $resultRecentNews = mysql_query("SELECT * FROM News WHERE NewsPinned = 0 AND NewsHidden = 0 ORDER by NewsID DESC") or die(mysql_error());
 
                         while($queryRecentNews = mysql_fetch_array($resultRecentNews)) {
                             echo "<a href='readNews.php?NewsID=$queryRecentNews[NewsID]' class='list-group-item'>".
@@ -38,7 +42,7 @@
                 <div class="list-group">
 
                     <?php
-                        $resultPinnedNews = mysql_query("SELECT * FROM News WHERE NewsPinned = 1 ORDER by NewsID DESC") or die(mysql_error());
+                        $resultPinnedNews = mysql_query("SELECT * FROM News WHERE NewsPinned = 1 AND NewsHidden = 0 ORDER by NewsID DESC") or die(mysql_error());
 
                         while($queryPinnedNews = mysql_fetch_array($resultPinnedNews)) {
                             echo "<a href='readNews.php?NewsID=$queryPinnedNews[NewsID]' class='list-group-item'>".
